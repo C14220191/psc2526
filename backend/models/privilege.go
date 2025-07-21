497e73c7f832d29a717f12a8ffa31dc7c82d4f93
KHgamerV4 <kevinhartono4@yahoo.co.id>
2025-07-21 20:12:07 +0700

    Update privilege.go

--- backend/models/privilege.go
+++ backend/models/privilege.go
@@ -4,6 +4,8 @@ import "time"
 
 type Privilege struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
+	NamaPrivilege string    `json:"nama_privilege"` // Nama privilege, misal "admin", "petugas", "mitra"
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at,omitempty"` // Waktu penghapusan, jika ada
 }
